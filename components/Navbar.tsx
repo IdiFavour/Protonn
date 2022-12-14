7fc993d90ce461cadc39a2d6764a66f3c043fa9c
Idi Favour <idifavour7@gmail.com>
2022-12-14 23:48:56 +0100

    finished navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
+import Image from 'next/image'
 const Navbar = () => {
     return ( 
         <nav className="navbar navbar-expand-lg">
-            <div className="container p-4 ">
+            <div className="container p-3 ">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <a className="navbar-brand" href="/">Protonn</a>
+                <Image className="navbar-brand" src="/Protonn.png" width={130} height={80} alt={'Brand Logo'}/>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
@@ -24,7 +25,7 @@ const Navbar = () => {
                         <li className="nav-item me-4">
                             <a className="nav-link" href="/">Resources</a>
                         </li>
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                        <button className="btn btn-main" type="submit">Get Started</button>
                         
                     </ul>
                 </div>
